5802bc684bb2fed53b2d5e16a9b81127efe8218d
Hoovered <brannon.hammer@yahoo.com>
2018-03-02 23:54:49 -0800

    cuck

--- FSetup 2.0/start.bat
+++ FSetup 2.0/start.bat
@@ -2,7 +2,7 @@
 
 :loop
 
-java -server -XX:MaxPermSize=512m -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=4 -XX:+AggressiveOpts -Xmx4096M -jar spigot.jar -o false 
+java -server -XX:MaxPermSize=512m -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=4 -XX:+AggressiveOpts -Xmx4096M -jar spigot.jar
 
 echo Press control+C to exit the server!
 
